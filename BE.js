bdb7124576994b07878eced9c52be0b6c5132629
EJoportogetino <julian.oporto.251@gmail.com>
2023-09-03 23:33:30 -0300

    0.03v

--- BE.js
+++ BE.js
@@ -1,26 +1,41 @@
 document.addEventListener("DOMContentLoaded", function()
   {
-    const form = document.getElementbyId("Calculador");
+    const form = document.getElementById("calculador");
 
-    form.addEventListener("submit", function (event)
+    form.addEventListener("submit", function(event)
     {
       event.preventDefault();
 
-      let num1= document.getElementbyId("primer");
-      let num2= document.getElementbyId("segundo");
-      let signo= document.getElementbyid("signo");
+      let num1= parseInt(document.getElementById("primer").value);
+      let num2= parseInt(document.getElementById("segundo").value);
+      let signo= document.getElementById("signo").value;
+      let resu;console.log(num1, num2, signo);
       switch (signo)
         {
           case "+":
-           let resu= num1 + num2;
-           alert ("Resultado de la suma: ", resu);
-          
-
+           resu= num1 + num2;
+           alert("Resultado de la suma: " + resu);
+           break;
+          case "-":
+           resu = num1- num2;
+           alert("Resultado de la resta: " + resu);
+           break;
+          case "*":
+            resu= num1*num2;
+            alert("Resultado de la multiplicacion: "+ resu);
+            break;
+          case "/":
+            if (num2 === 0)
+              {
+                alert("Capo, Como vas a dividir por 0?");
+                break;
+              }else {
+                resu = Math.floor(num1/num2);
+                alert("El resultado de la division es: " + resu);
+              }
         }
 
+      });
 
-    }
 
-  )
-  }
-)
+  });
